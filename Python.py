629c9fd8d5ccb1f30ad53c6b1793c2a163fec5c2
Ian Haddock <ianhaddock99@gmail.com>
2020-11-10 21:15:11 -0500

    Python exercise 1 - 5

---
+++ Python.py
@@ -0,0 +1,59 @@
+#1. Hello You!
+#name = input("What is your name?")
+#print("Hello " + name + "!")
+
+#2. HELLO YOU!
+#name = input("What is your name?")
+#print("Hello " + name.upper() + "!")
+#print("Your name has", len(name), "letters in it!")
+
+#3. Madlib
+#print("Fill in the blanks below:\n(Name) ran towards the (Animal).")
+#name = input("What is the name?")
+#animal = input("What is the animal?")
+#print("%s ran towards the %s." % (name, animal))
+
+#4. Day of the Week
+#day = int(input('Day (0-6)? '))
+#if day == 0:
+    #print("Sunday")
+#elif day == 1:
+    #print("Monday")
+#elif day == 2:
+    #print("Tuesday")
+#elif day == 3:
+    #print("Wednesday")
+#elif day == 4:
+    #print("Thursday")
+#elif day == 5:
+    #print("Friday")
+#elif day == 6:
+    #print("Saturday")
+#else:
+    #print("Not a valid number")
+
+
+
+#5 Work or Sleep In
+#day = int(input('Day (0-6)? '))
+#if day == 0:
+    #print("Sleep in")
+#elif day == 1:
+    #print("Go to work")
+#elif day == 2:
+    #print("Go to work")
+#elif day == 3:
+    #print("Go to work")
+#elif day == 4:
+    #print("Go to work")
+#elif day == 5:
+    #print("Go to work")
+#elif day == 6:
+    #print("Sleep in")
+#else:
+    #print("You did not enter a number between 0 and 6")
+
+
+#6 Celsius to Fahrenheit
+
+
